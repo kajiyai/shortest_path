ebae50ad0ecc0e5f98386aca6f85cf9fb32aacbe
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-12-09 14:21:41 +0900

    各経過を視覚的に見やすくしてもらった.by GPT

--- short_path_without_wall.rb
+++ short_path_without_wall.rb
@@ -20,28 +20,29 @@ end
 class Node
     attr_accessor :cost, :current, :queue, :goal
   
-    def initialize(start, goal)
-      @cost = Hash.new(Float::INFINITY) # 各ノードへのコストを無限大で初期化
+    def initialize(start, goal, grid_size_x, grid_size_y)
+      @grid_size_x = grid_size_x
+      @grid_size_y = grid_size_y
+      @cost = Hash.new(Float::INFINITY)
       @current = start
       @goal = goal
-      @queue = [start] # スタート位置をキューに追加
-      @cost[start] = 0 # スタート位置のコストは0
+      @queue = [start]
+      @cost[start] = 0
     end
   
     def find_path
       until queue.empty?
-        self.current = queue.shift # キューからノードを取り出す
-        return cost[current] if current == goal # ゴールに到達した場合
+        self.current = queue.shift
   
-        puts "現在のノード: #{current}"
-        puts "現在のキュー: #{queue}"
-        puts "各ノードへのコスト: #{cost}"
+        display_grid # グリッドを表示
+  
+        return cost[current] if current == goal
   
         access_adjacent(current).each do |adj_node|
-          next_cost = cost[current] + 1 # 隣接ノードへの移動コストを1とする
+          next_cost = cost[current] + 1
           if next_cost < cost[adj_node]
             cost[adj_node] = next_cost
-            queue.push(adj_node)
+            queue.push(adj_node) unless queue.include?(adj_node)
           end
         end
       end
@@ -50,21 +51,47 @@ class Node
   
     private
   
+    def display_grid
+      puts "\nグリッドの状態:"
+      @grid_size_x.times do |x|
+        @grid_size_y.times do |y|
+          print_node([x, y])
+        end
+        puts
+      end
+      puts "現在のキュー: #{queue}"
+    end
+  
+    def print_node(node)
+      if node == current
+        print " C " # 現在のノード
+      elsif node == goal
+        print " G " # ゴール
+      elsif queue.include?(node)
+        print " Q " # キューに含まれるノード
+      else
+        cost = @cost[node]
+        print cost.infinite? ? " . " : " #{cost} "
+      end
+    end
+  
     def access_adjacent(node)
       x, y = node
       [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]].select { |nx, ny| valid_node?(nx, ny) }
     end
   
     def valid_node?(x, y)
-      x >= 0 && y >= 0 # 有効なノードの条件
+      x >= 0 && y >= 0 && x < @grid_size_x && y < @grid_size_y
     end
   end
   
-  # スタートとゴールの位置設定
-  start = [0, 0] # スタート位置
-  goal = [2, 2]  # ゴール位置
+  # スタートとゴールの位置設定、グリッドサイズの設定
+  start = [0, 0]
+  goal = [2, 4]
+  grid_size_x = 3
+  grid_size_y = 5
   
   # インスタンス化と経路探索
-  node = Node.new(start, goal)
+  node = Node.new(start, goal, grid_size_x, grid_size_y)
   puts "最短コスト: #{node.find_path}"
   
